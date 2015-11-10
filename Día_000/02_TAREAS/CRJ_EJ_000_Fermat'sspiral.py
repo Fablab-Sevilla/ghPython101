2c3aa3c6b4713993417ff945e46dd616a634d0b5
evvicente <YrFHxmoM3cY8MWioqMIN>
2015-11-10 13:14:23 +0100

    CRJ_EJ_000_Primer_envio
    Practica de Cristina

---
+++ Día_000/02_TAREAS/CRJ_EJ_000_Fermat'sspiral.py
@@ -0,0 +1,26 @@
+﻿#importar librerias
+import rhinoscriptsyntax as rs
+import math as m
+import ghpythonlib.components as gh
+
+#lista vacia puntos
+listPts = []
+#radio esfera
+r = C*m.sqrt((It-1)*Alpha) 
+
+#bucle espiral-esfera
+for i in range(It):
+    angTheta = i*Alpha
+    rh = C*m.sqrt(angTheta)
+    pt = rs.Polar(ptB,angTheta,rh)
+    #lista puntos planos
+    listPts.append(pt)
+    
+    #rv = sqrt(r^2-rh^2) pitágoras
+    rv = m.sqrt(m.pow(r,2)-m.pow(rh,2))
+    #sustituir la Z de cada punto por su rv
+    listPts[i].Z=rv
+    print listPts
+
+Pts = listPts 
+#vor = gh.Voronoi(Pts,rad)
