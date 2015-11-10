802101d0531f5482b9fd9a770681201ae9b4752c
JosBuz <josebuzongonzalez@gmail.com>
2015-11-10 13:11:13 +0100

    JBG_sin esfera
    He copiado el archivo del profe y cambiado el nombre :(, no hice las
    tareas.

---
+++ Día_000/02_TAREAS/JBG_EJ_000_nada_nuevo.py
@@ -0,0 +1,13 @@
+﻿import rhinoscriptsyntax as rs
+import math as m
+import ghpythonlib.components as gh
+
+auxPts = []
+
+for i in range(It):
+    angTheta = i*Alpha
+    pt = rs.Polar(ptB,angTheta,C*m.sqrt(angTheta))
+    auxPts.append(pt)
+    
+Pts = auxPts 
+vor = gh.Voronoi(Pts,rad)
