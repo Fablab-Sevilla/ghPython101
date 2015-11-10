951000f651803afbe5c8e13c60d25fc2ab5bb318
prokaos <arquitecto@andrescabrera.es>
2015-11-10 13:13:24 +0100

    Prueba ejercicio primero

---
+++ Día_000/02_TAREAS/ACC_EJ_000.py
@@ -0,0 +1,16 @@
+﻿import rhinoscriptsyntax as rs
+import math as m
+import ghpythonlib.components as gh
+
+auxPts = []
+for i in range(It):
+    angTheta = i*Alpha
+    angFi = i*Betha
+    x = C*m.sqrt(angTheta)*m.sqrt(angFi)*m.sin(angTheta)*m.cos(angFi)
+    y = C*m.sqrt(angTheta)*m.sqrt(angFi)*m.sin(angTheta)*m.sin(angFi)
+    z = C*m.sqrt(angTheta)*m.sqrt(angFi)*m.cos(angTheta)
+    pt = rs.AddPoint(x,y,z)
+    auxPts.append(pt)
+
+Pts = auxPts 
+#dela = gh.Delaunay(Pts)
