bb14b94c46990f209d2c833aaa2ba80a3fd530cc
Kraken (Sergi) <sergioeviel@gmail.com>
2015-11-11 08:53:05 +0100

    SV_EJ01_v02
    Revisión del código. Versión 2

--- Día_001/01_EJ/T_002/SV_EJ01.py
+++ Día_001/01_EJ/T_002/SV_EJ01.py
@@ -1,39 +1,49 @@
-﻿#importamos bibliotecas
-import rhinoscriptsyntax as rs
+﻿"""Alumno: Sergi Viel
+    usuario: evokraken
+    version 02.00
+"""
+"""
+CONTROL DE CAMBIOS
+1-Desabilitamos las bibliotecas que no estamos usando. Usamos # coment. Lin-15
+2-Usamos el metodo randint(), e inhabilitamos la funcion aMod(). Inhabilitamos
+la funcion con comentario. Lin-20
+3-Incluimos la obtencion de variables del vector y obtenemos directamente el vector
+saltandonos un paso. Lin 39-40 incluidas en Lin 43.
+
+"""
+
+#importamos bibliotecas
+#import rhinoscriptsyntax as rs
 import random as rd
 import Rhino.Geometry as rg
 
 #creamos una función que nos crea el módulo aleatorio a valores regulados
-def aMod(valor):
-    #print valor
-    if valor <= 0.3:
-        return -1
-    elif valor > 0.3 and valor <= 0.6:
-        return 0
-    elif valor > 0.6:
-        return 1
-    else:
-        #No debe salir nunca este mensaje, es como control de error
-        print ("Entrada fuera de rango")
+###def aMod(valor):
+###    #print valor
+###    if valor <= 0.3:
+###        return -1
+###    elif valor > 0.3 and valor <= 0.6:
+###        return 0
+###    else:
+###        return 1
 
 #configuramos random para un seed aportado por usuario.
 rd.seed(int(sSeed))
 
-#punto origen aportado por usuario y un objeto acumulador de los trazos
+#punto origen aportado por usuario y acumulador de resultados
 origen = rg.Point3d(PtoO)
 resultado=[]
 
 for i in range(intN):
     #coordenadas de vector
-    vx= aMod(rd.random())
-    vy= aMod(rd.random())
+    #vx= rd.randint(-1,1)
+    #vy= rd.randint(-1,1)
     
     #vector de recta
-    VtoF = rg.Vector3d(vx,vy,0)
+    VtoF = rg.Vector3d(rd.randint(-1,1),rd.randint(-1,1),0)
     
     #trazo de la recta final
     trazo=rg.Line(origen, VtoF)
-    #acumulamos el resultado para la salida final
     resultado.append(trazo)
     
     
@@ -41,5 +51,5 @@ for i in range(intN):
     punto = rg.Point3d(origen+VtoF)
     origen = rg.Point3d(punto)
     
-#suma de todos los trazos lo dejamos preparado para la salida
+#suma de todos los trazos lo dejamos para la salida
 a = resultado
