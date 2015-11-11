c9eff0cfc607cb3bca74f8e0f22601a8585f9c26
Kraken (Sergi) <sergioeviel@gmail.com>
2015-11-11 15:07:19 +0100

    SV_EJ01_v03
    Version 03, simplificando codigo.
    Aporte de imagen de captura.

--- Día_001/01_EJ/T_002/SV_EJ01.py
+++ Día_001/01_EJ/T_002/SV_EJ01.py
@@ -1,44 +1,27 @@
 ﻿"""Alumno: Sergi Viel
     usuario: evokraken
-    version 02.00
+    version 03.00
 """
 """
 CONTROL DE CAMBIOS
-1-Desabilitamos las bibliotecas que no estamos usando. Usamos # coment. Lin-15
-2-Usamos el metodo randint(), e inhabilitamos la funcion aMod(). Inhabilitamos
-la funcion con comentario. Lin-20
-3-Incluimos la obtencion de variables del vector y obtenemos directamente el vector
-saltandonos un paso. Lin 39-40 incluidas en Lin 43.
+1-Borra y limpiar comentarios e instrucciones comentadas de versiones anteriores
+2-Forzar Type Hint del componente de GH. Lin-19
+3-Forzamos la variable PtoO Point3d en Type Hint del componente. Ya no tenemos
+que tipificar en lineas 21, 34 y 35.
 
 """
 
-#importamos bibliotecas
-#import rhinoscriptsyntax as rs
 import random as rd
 import Rhino.Geometry as rg
 
-#creamos una función que nos crea el módulo aleatorio a valores regulados
-###def aMod(valor):
-###    #print valor
-###    if valor <= 0.3:
-###        return -1
-###    elif valor > 0.3 and valor <= 0.6:
-###        return 0
-###    else:
-###        return 1
-
 #configuramos random para un seed aportado por usuario.
-rd.seed(int(sSeed))
+rd.seed(sSeed)
 
 #punto origen aportado por usuario y acumulador de resultados
-origen = rg.Point3d(PtoO)
+origen = (PtoO)
 resultado=[]
 
 for i in range(intN):
-    #coordenadas de vector
-    #vx= rd.randint(-1,1)
-    #vy= rd.randint(-1,1)
-    
     #vector de recta
     VtoF = rg.Vector3d(rd.randint(-1,1),rd.randint(-1,1),0)
     
@@ -48,8 +31,8 @@ for i in range(intN):
     
     
     #preparamos para el siguiente bucle
-    punto = rg.Point3d(origen+VtoF)
-    origen = rg.Point3d(punto)
+    punto = (origen+VtoF)
+    origen = (punto)
     
 #suma de todos los trazos lo dejamos para la salida
 a = resultado
