dd7251dc1a555bcf7b21c71d03518c7e90766ca3
cris84rj <cris84rj@gmail.com>
2015-11-12 12:24:18 +0100

    CRJ_EJ_002_RandomWalkerModified
    Random de polígonos.
    Variables: número de lados (n) y radio (r).

---
+++ Día_002/01_EJ/T_000/CRJ_EJ_002_RandomWalkerModified.py
@@ -0,0 +1,33 @@
+﻿#CRJ_RandomWalker_Modified
+
+#librerias
+import Rhino.Geometry as rg
+import math as m
+import random as rd
+import rhinoscriptsyntax as rs
+
+#semilla
+rd.seed(s)
+
+#variable centro poligono
+centro=pt0
+
+#bucle
+for i in range (It):
+    #dibuja un círculo con centro pt0 y radio r
+    circle = rg.Circle(centro,r)
+    #convertir circulo en curva
+    curve = circle.ToNurbsCurve()
+    #divide la curva en n partes
+    points = rs.DivideCurve(curve,n,False,True)
+    #añadir a la lista su primer punto
+    points.append(points[0])
+    #dibuja lineas
+    lines = rs.AddPolyline(points)
+
+
+
+
+
+
+
