19825b70d6f15e995a598700a7c5990a0f6d479e
Ángel Linares García <angel@monzon8.es>
2015-11-18 23:35:38 +0100

    Working with function
    Function added...no recursive.

--- Día_004/01_EJ/Polyhedra_Subd.py
+++ Día_004/01_EJ/Polyhedra_Subd.py
@@ -10,9 +10,9 @@ newMeshes = []
 # We need to compute mesh normals before accessing them
 M.Normals.ComputeNormals()
 
-
-
-for i in range(M.Faces.Count):
+def subDMesh(M,gen):
+    
+    for i in range(M.Faces.Count):
     meshNew = rg.Mesh()
     vertex = M.Faces.GetFaceVertices(i)
     center = M.Faces.GetFaceCenter(i)
@@ -32,26 +32,42 @@ for i in range(M.Faces.Count):
     
     meshNew.Vertices.Add(center)
     
-    # Caras de 4 lados
-    if len(vertex)==5:
     
-        meshNew.Faces.AddFace(0,1,4)
-        meshNew.Faces.AddFace(1,2,4)
-        meshNew.Faces.AddFace(2,3,4)
-        meshNew.Faces.AddFace(3,0,4)
-    
-    # Caras de 3 lados    
-    elif len(vertex)==4:
+    for k in range(len(vertex)-1):
         
-        meshNew.Faces.AddFace(0,1,4)
-        meshNew.Faces.AddFace(1,2,4)
-        meshNew.Faces.AddFace(2,3,4)
+        if k == len(vertex)-2:
+            k1 = 0
+        else: 
+            k1 = k+1
+            
+        meshNew.Faces.AddFace(k,k1,4)
+    
+    
+#     # Caras de 4 lados
+#     if len(vertex)==5:
+#     
+#         meshNew.Faces.AddFace(0,1,4)
+#         meshNew.Faces.AddFace(1,2,4)
+#         meshNew.Faces.AddFace(2,3,4)
+#         meshNew.Faces.AddFace(3,0,4)
+#     
+#     # Caras de 3 lados    
+#     elif len(vertex)==4:
+#         
+#         meshNew.Faces.AddFace(0,1,4)
+#         meshNew.Faces.AddFace(1,2,4)
+#         meshNew.Faces.AddFace(2,0,4)
     
     meshNew.Normals.ComputeNormals()
     meshNew.Compact()
     
     newMeshes.append(meshNew)
     centerList.append(center)
+    
+    pass
+    
+
+
     
 a = newMeshes
     
---
+++ Día_004/01_EJ/Polyhedra_Subd_rec.py
@@ -0,0 +1,78 @@
+import Rhino.Geometry as rg
+import ghpythonlib.components as gh
+
+#converting the brep into a simple mesh
+M = gh.SimpleMesh(Brep)
+
+centerList = []
+newMeshes = rg.Mesh()
+
+# We need to compute mesh normals before accessing them
+M.Normals.ComputeNormals()
+
+def subDMesh(M,dist,gen):
+    
+    for i in range(M.Faces.Count):
+        meshNew = rg.Mesh()
+        vertex = M.Faces.GetFaceVertices(i)
+        center = M.Faces.GetFaceCenter(i)
+        normal = M.FaceNormals.Item[i]
+    
+        #Unitizing normal vector
+        normalU = normal
+        normalU.Unitize()
+    
+        #Moving the face center
+        center = center + (normalU.Multiply(normalU,dist))
+
+        for j in range(len(vertex)-1):
+        
+            meshNew.Vertices.Add(vertex[j+1])
+    
+    
+        meshNew.Vertices.Add(center)
+    
+    
+        for k in range(len(vertex)-1):
+        
+            if k == len(vertex)-2:
+                k1 = 0
+            else: 
+                k1 = k+1
+            
+            meshNew.Faces.AddFace(k,k1,4)
+    
+#         #Traducción manual del loop k
+#         # Caras de 4 lados
+#         if len(vertex)==5:
+#          
+#             meshNew.Faces.AddFace(0,1,4)
+#             meshNew.Faces.AddFace(1,2,4)
+#             meshNew.Faces.AddFace(2,3,4)
+#             meshNew.Faces.AddFace(3,0,4)
+#          
+#         # Caras de 3 lados    
+#         elif len(vertex)==4:
+#              
+#             meshNew.Faces.AddFace(0,1,4)
+#             meshNew.Faces.AddFace(1,2,4)
+#             meshNew.Faces.AddFace(2,0,4)
+    
+        meshNew.Normals.ComputeNormals()
+        meshNew.Compact()
+    
+        newMeshes.Append(meshNew)
+        centerList.append(center)
+        
+    return newMeshes
+
+
+
+    
+a = subDMesh(M, dist, gen)
+
+    
+    
+    
+
+    
