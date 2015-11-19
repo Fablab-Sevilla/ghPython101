b6e11a3a552e48e3f2a91bf4445f18bf7e5b1b6b
osfa <perezdelama@us.es>
2015-11-19 10:46:52 +0100

    panelado con funcion panel
    no llego a generar un panel diferente

---
+++ Día_004/02_TAREAS/jpl_panelado_superficie_con_funcion_panel.py
@@ -0,0 +1,37 @@
+﻿import Rhino.Geometry as rg
+
+def panelGen(pt0,pt1,pt2,pt3):
+    pan = rg.PolylineCurve([pt0,pt1,pt2,pt3,pt0])
+
+    return pan
+    
+#print panel
+
+ptList = []
+polList = []
+
+for i in range(uDiv+1):
+    ptListTemp = []
+    for j in range(vDiv+1):
+        
+        #print "puntos " + str(i) + "." + str(j)
+        #atencion superficie reparametrizada en gh
+        ptTemp = srf.Evaluate(i/uDiv,j/vDiv,2)[1]
+        ptListTemp.append(ptTemp)
+        
+    ptList.append(ptListTemp)
+    #print ptList
+
+for i in range(uDiv):
+    for j in range(vDiv):
+        
+        pt0a = ptList[i][j]
+        pt1a = ptList[i+1][j]
+        pt2a = ptList[i+1][j+1]
+        pt3a = ptList[i][j+1]
+        
+        panel = panelGen(pt0a,pt1a,pt2a,pt3a)
+        
+        polList.append(panel)
+        
+panels = polList
